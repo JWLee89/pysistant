a92f63ad64386e963b0c7102c56e3d662534b467
Jay Lee <ljay189@gmail.com>
2019-12-23 11:20:53 +0900

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,28 @@
     @Author Jay Lee
     Pysistant setup file.
 """
-from setuptools import setup
+import setuptools
 
-setup(name='Pysistant',
+
+with open("README.md", 'r') as readme:
+      long_description = readme.read()
+
+
+setuptools.setup(
+      name='Pysistant',
       version='0.0.1',
       description='A simple library of utility functions',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/JWLee89/pysistant',
+      packages=setuptools.find_packages(),
+      classifiers=[
+           "Programming Language :: Python :: 3",
+           "License :: OSI Approved :: MIT License",
+           "Operating System :: OS Independent",
+      ],
       author='Jay Lee',
       author_email='ljay189@gmail.com',
       license='MIT',
-      packages=['deeplearning', 'file', 'logging', 'matplotlib', 'os', 'util'],
+      python_requires='>=3.5',
       zip_safe=False)
